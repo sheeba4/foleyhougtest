69cadbfba637919f49fdc4a4561d5bac3966dfba
ndevr-codeship2 <codeship@ndevr.io>
2016-05-31 14:28:31 +0000

    Deployment for foleyhoag staging

--- wp-content/themes/fohopoco/core/functions/functions.php
+++ wp-content/themes/fohopoco/core/functions/functions.php
@@ -233,25 +233,13 @@ function ShortenText($text)
 /*******************************************************************************
  jQuery from google and move js to footer
 ********************************************************************************/
-
-$url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'; // the URL to check against
-$test_url = @fopen($url,'r'); // test parameters
-if($test_url !== false) { // test if the URL exists
-
-	function load_external_jQuery() { // load external file
-		wp_deregister_script( 'jquery' ); // deregisters the default WordPress jQuery
-		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'); // register the external file
-		wp_enqueue_script('jquery'); // enqueue the external file
-	}
-	add_action('wp_enqueue_scripts', 'load_external_jQuery'); // initiate the function
-} else {
-	function load_local_jQuery() {
-		wp_deregister_script('jquery'); // initiate the function
-		wp_register_script('jquery', bloginfo('template_url').'/_ui/js/jquery-1.7.1.min.js', __FILE__, false, '1.7.1', true); // register the local file
-		wp_enqueue_script('jquery'); // enqueue the local file
-    }
-	add_action('wp_enqueue_scripts', 'load_local_jQuery'); // initiate the function
+function load_local_jQuery() {
+	wp_deregister_script('jquery'); // initiate the function
+	wp_register_script('jquery', get_template_directory_uri().'/_ui/js/jquery-1.7.1.min.js', __FILE__, false, '1.7.1', true); // register the local file
+	wp_enqueue_script('jquery'); // enqueue the local file
 }
+add_action('wp_enqueue_scripts', 'load_local_jQuery'); // initiate the function
+
 
 
 remove_action('wp_head', 'wp_print_scripts');
