3d7d9d468d4ad4d876a4371d60f1fc7fe7ece449
Meeky Hwang <meekyhwang@gmail.com>
2016-09-22 13:07:02 -0400

    excerpt test

--- themes/fohopoco/core/functions/functions.php
+++ themes/fohopoco/core/functions/functions.php
@@ -294,40 +294,109 @@ function kpl_user_bio_visual_editor_unfiltered() {
 	remove_all_filters('pre_user_description');
 }
 add_action('admin_init','kpl_user_bio_visual_editor_unfiltered');
+//
+//function fohopoco_wp_trim_excerpt( $text ) {
+//	$raw_excerpt = $text;
+//	if ( '' == $text ) {
+//		$text = get_the_content('');
+//
+//		$text = strip_shortcodes( $text );
+//
+//
+//		$text = apply_filters('the_content', $text);
+//		$text = str_replace(']]>', ']]&gt;', $text);
+//		$text = strip_tags( $text, '<p><a><img>' );
+//
+//		$excerpt_length = apply_filters('excerpt_length', 100);
+//		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+//		$words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
+//		if ( count($words) > $excerpt_length ) {
+//			array_pop($words);
+//			$text = implode(' ', $words);
+//			$text = $text . $excerpt_more;
+//		} else {
+//			$text = implode(' ', $words);
+//		}
+//	}
+//	return $text;
+//}
+//remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+//add_filter('get_the_excerpt', 'fohopoco_wp_trim_excerpt');
+//
+//function fohopoco_excerpt_more( $more ) {
+//	global $post;
+//	return '... <a href="' . get_permalink( $post->ID ) .'">More</a>';
+//}
+//add_filter('excerpt_more', 'fohopoco_excerpt_more');
 
-function fohopoco_wp_trim_excerpt( $text ) {
-	$raw_excerpt = $text;
-	if ( '' == $text ) {
-		$text = get_the_content('');
 
-		$text = strip_shortcodes( $text );
 
-		
-		$text = apply_filters('the_content', $text);
-		$text = str_replace(']]>', ']]&gt;', $text);
-		$text = strip_tags( $text, '<p><a><img>' );
-		
-		$excerpt_length = apply_filters('excerpt_length', 100);
-		$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
-		$words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
-		if ( count($words) > $excerpt_length ) {
-			array_pop($words);
-			$text = implode(' ', $words);
-			$text = $text . $excerpt_more;
-		} else {
-			$text = implode(' ', $words);
+
+function wpse_allowedtags() {
+	// Add custom tags to this string
+	return '<script>,<style>,<br>,<em>,<i>,<ul>,<ol>,<li>,<a>,<p>,<img>,<video>,<audio>';
+}
+
+if ( ! function_exists( 'wpse_custom_wp_trim_excerpt' ) ) :
+
+	function wpse_custom_wp_trim_excerpt($wpse_excerpt) {
+		$raw_excerpt = $wpse_excerpt;
+		if ( '' == $wpse_excerpt ) {
+
+			$wpse_excerpt = get_the_content('');
+			$wpse_excerpt = strip_shortcodes( $wpse_excerpt );
+			$wpse_excerpt = apply_filters('the_content', $wpse_excerpt);
+			$wpse_excerpt = str_replace(']]>', ']]&gt;', $wpse_excerpt);
+			$wpse_excerpt = strip_tags($wpse_excerpt, wpse_allowedtags()); /*IF you need to allow just certain tags. Delete if all tags are allowed */
+
+			//Set the excerpt word count and only break after sentence is complete.
+			$excerpt_word_count = 75;
+			$excerpt_length = apply_filters('excerpt_length', $excerpt_word_count);
+			$tokens = array();
+			$excerptOutput = '';
+			$count = 0;
+
+			// Divide the string into tokens; HTML tags, or words, followed by any whitespace
+			preg_match_all('/(<[^>]+>|[^<>\s]+)\s*/u', $wpse_excerpt, $tokens);
+
+			foreach ($tokens[0] as $token) {
+
+				if ($count >= $excerpt_length && preg_match('/[\,\;\?\.\!]\s*$/uS', $token)) {
+					// Limit reached, continue until , ; ? . or ! occur at the end
+					$excerptOutput .= trim($token);
+					break;
+				}
+
+				// Add words to complete sentence
+				$count++;
+
+				// Append what's left of the token
+				$excerptOutput .= $token;
+			}
+
+			$wpse_excerpt = trim(force_balance_tags($excerptOutput));
+
+			$excerpt_end = '... <a href="'. esc_url( get_permalink() ) . '">More</a>';
+			$excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end);
+
+			//$pos = strrpos($wpse_excerpt, '</');
+			//if ($pos !== false)
+			// Inside last HTML tag
+			//$wpse_excerpt = substr_replace($wpse_excerpt, $excerpt_end, $pos, 0); /* Add read more next to last word */
+			//else
+			// After the content
+			$wpse_excerpt .= $excerpt_more; /*Add read more in new paragraph */
+
+			return $wpse_excerpt;
+
 		}
+		return apply_filters('wpse_custom_wp_trim_excerpt', $wpse_excerpt, $raw_excerpt);
 	}
-	return $text;
-}
-remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'fohopoco_wp_trim_excerpt');
 
-function fohopoco_excerpt_more( $more ) {
-	global $post;
-	return '... <a href="' . get_permalink( $post->ID ) .'">More</a>';
-}
-add_filter('excerpt_more', 'fohopoco_excerpt_more');
+endif;
+
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'wpse_custom_wp_trim_excerpt');
 
 
 add_action( 'save_post', 'fohopoco_save_metadata' );
