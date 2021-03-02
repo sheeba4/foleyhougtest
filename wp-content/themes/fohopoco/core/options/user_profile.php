187e310e38df0e40b0cb204bb88410144027625f
Matthew Dorman <mdorman@ndevr.io>
2021-03-02 16:57:27 -0700

    standards cleanup

--- wp-content/themes/fohopoco/core/options/user_profile.php
+++ wp-content/themes/fohopoco/core/options/user_profile.php
@@ -16,14 +16,18 @@ class User_Profile {
 	 */
 	function admin_scripts() {
 		wp_enqueue_media();
-		wp_register_script('foley_user', get_template_directory_uri() . '/_ui/js/user_profile.admin.js', array('jquery','media-upload','thickbox'), '1.0.3' );
-		wp_enqueue_script('foley_user');
+		wp_register_script( 'foley_user', get_template_directory_uri() . '/_ui/js/user_profile.admin.js', array(
+			'jquery',
+			'media-upload',
+			'thickbox'
+		), '1.0.3' );
+		wp_enqueue_script( 'foley_user' );
 	}
 
 	/**
 	 * @param $user
 	 */
-	public function render_user_form( $user ){ ?>
+	public function render_user_form( $user ) { ?>
 		<h3>Profile Image Override</h3>
 
 		<table class="form-table">
@@ -31,22 +35,26 @@ class User_Profile {
 				<th><label for="profile_image"><?php esc_html_e( 'File Url:', 'fohopoco' ); ?></label></th>
 				<?php $profile_image = get_the_author_meta( 'profile_image', $user->ID ); ?>
 
-				<td><img src="<?php echo esc_url($profile_image); ?>" /><td/>
+				<td><img src="<?php echo esc_url( $profile_image ); ?>"/>
+				<td/>
 				<td>
-					<input type="text" class="text" size="60" name="profile_image" id="profile_image" value="<?php echo esc_url( $profile_image );?>">
-					<input type="button" id="profile_image-button" class="button" value="<?php _e( 'Choose or Upload a file' );?>">
+					<input type="text" class="text" size="60" name="profile_image" id="profile_image"
+						   value="<?php echo esc_url( $profile_image ); ?>">
+					<input type="button" id="profile_image-button" class="button"
+						   value="<?php _e( 'Choose or Upload a file' ); ?>">
 					<p class="description">Please enter your url for the image or upload</p>
 				</td>
 			</tr>
 			<tr>
 				<?php $hide_sidebar = get_the_author_meta( 'hide_sidebar', $user->ID ); ?>
 				<td>
-					<input type="checkbox" class="checkbox" name="hide_sidebar" id="hide_sidebar" value="hide" <?php checked($hide_sidebar, 'hide');?>>
+					<input type="checkbox" class="checkbox" name="hide_sidebar" id="hide_sidebar"
+						   value="hide" <?php checked( $hide_sidebar, 'hide' ); ?>>
 					<label for="hide_sidebar"><?php esc_html_e( 'Hide from author widgets', 'fohopoco' ); ?></label>
 				</td>
 			</tr>
 		</table>
-	<?php
+		<?php
 	}
 
 	/**
@@ -54,11 +62,12 @@ class User_Profile {
 	 *
 	 * @return bool
 	 */
-	public function save_user_form( $user_id ){
-		if ( !current_user_can( 'edit_user', $user_id ) )
+	public function save_user_form( $user_id ) {
+		if ( ! current_user_can( 'edit_user', $user_id ) ) {
 			return false;
+		}
 
-		if( isset($_POST['hide_sidebar'] )){
+		if ( isset( $_POST['hide_sidebar'] ) ) {
 			update_usermeta( $user_id, 'hide_sidebar', $_POST['hide_sidebar'] );
 		} else {
 			update_usermeta( $user_id, 'hide_sidebar', '' );
@@ -68,7 +77,7 @@ class User_Profile {
 
 }
 
-add_action('admin_init', 'user_profile');
+add_action( 'admin_init', 'user_profile' );
 
 function user_profile() {
 	new User_Profile();
